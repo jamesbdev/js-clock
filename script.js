fe3a113c04bd9a444e743fc3e7af704d377c12b3
jaybaybremner <j.bremner@ymail.com>
2020-12-31 14:29:43 +0000

    adding alarm checking

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const date = new Date();
+const dateContainer = document.getElementById('date-container');
+const time = date.getHours() + ":" + date.getMinutes();
 
 function updateTime(){
-    const date = new Date();
-    const dateContainer = document.getElementById('date-container');
-    const time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
     dateContainer.innerHTML = time;
 }
 
+// check if the time set is equal to the date 
+function checkAlarm(){
+    const alarmTime = document.getElementById('alarm');
+    console.log(time);
+    if (alarmTime.value == time) {
+      alert('Time to wake up!');
+    }
+}
 setInterval(updateTime, 1000);
+checkAlarm();
+
+
+
+
+
